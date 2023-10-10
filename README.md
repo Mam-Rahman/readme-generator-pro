3ac3efee14ec0a470b035ab1380f1bc62ff6e6d6
Mamunur Rahman <mamunur@mac-air.home>
2023-10-10 18:31:22 +0100

    completed the readme javascript code in js files

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# readme-generator-pro
-A Readme Generator
+# README Generator Pro
+  ![Github license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+  ### Description
+
+  A readme generator written in Node.js
+
+  ### Table of Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributing](#contributing)
+
+  * [Test](#tests)
+
+  * [Questions](#questions)
+
+  ## Installation
+
+  To install necessary dependencies, run the following command:
+
+  ```
+  npm i
+  ```
+
+  ## Usage
+
+  Have Node.js installed
+
+  ## License
+
+  This project is licensed under the MIT license.
+
+  ## Contributing
+
+  fork
+
+  ## Tests
+
+  To run tests, run following command:
+
+  ```
+  npm test
+  ```
+
+  ### Questions
+
+  If you have any questions about the repo open an issue or contact me 
+  directly at mam@gmail.com.
+
+  You can find more of my work at [mam-rahman](https://github.com/mam-rahman).
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,74 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const renderLicenseBadge = (license) => {
+  if(license !== "None") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`
+  }
+  return ''
+}
 
+const renderLicenseLink = (license) => {
+  if(license !== "None") {
+    return `* [License](#license)\n`
+  }
+  return '';
+}
+
+function generateMarkdown(data) {
+  console.log(data);
+  return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+  ### Description
+
+  ${data.description}
+
+  ### Table of Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  ${renderLicenseLink(data.license)}
+  * [Contributing](#contributing)
+
+  * [Test](#tests)
+
+  * [Questions](#questions)
+
+  ## Installation
+
+  To install necessary dependencies, run the following command:
+
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  This project is licensed under the ${data.license} license.
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  To run tests, run following command:
+
+  \`\`\`
+  ${data.test}
+  \`\`\`
+
+  ### Questions
+
+  If you have any questions about the repo open an issue or contact me 
+  directly at ${data.email}.
+
+  You can find more of my work at [${data.github}](https://github.com/${data.github}).
 `;
 }
 
